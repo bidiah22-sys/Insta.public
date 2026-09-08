b043ec0e68b7f07f0f579bceef0fde23888c448f
bidiah22-sys <bidiah22@gmail.com>
2026-09-09 01:56:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ BOT_USERNAME = os.getenv("BOT_USERNAME") or "bot222703"
 OWNER_USERNAME = "fx_smw"
 
 # 🔑 अपनी सेशन आईडी यहाँ डाल दे भाई
-SESSION_ID = "YAHAN_APNI_SESSION_ID_DAL"
+SESSION_ID = "27413581604%3AouSmyrPKPDgZ9t%3A22%3AAYj1HtAB90lP104ze_KkGVJ9ZXQcu2lY-mCLoX8ZHAL"
 
 if not SESSION_ID or SESSION_ID == "YAHAN_APNI_SESSION_ID_DAL":
     raise RuntimeError("Critical Error: Instagram Session ID is missing!")
