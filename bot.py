d009cdc42bfbe716a3e7d8a1230ff6546ca693b0
bidiah22-sys <bidiah22@gmail.com>
2026-09-16 15:12:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from instagrapi import Client
 from instagrapi.exceptions import LoginRequired, ChallengeRequired
 
 # ==================== CONFIGURATION ====================
-SESSION_ID = os.getenv("INSTA_SESSION_ID") or "YAHAN_APNI_SESSION_ID_PASTE_KAR_DENA"
+SESSION_ID = "7207590267%3A0QpUpraqt4dWWn%3A6%3AAYmbdGY6kWOw5x0oYZJaGw3z96Ua_YgL5a7y0ikorA"
 
 BOT_USERNAME = "bot222703" # अपना बोट यूजरनेम यहाँ डालें
 OWNER_USERNAME = "fx_smw"
